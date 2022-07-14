db20ac4788c8d266ba48333db871b5327247a01b
ranazohaib <mdshoaibpk90@gmail.com>
2022-07-14 20:25:04 +0500

    HTML code

---
+++ landing.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+
+ * Template Name: Landing-10x Page
+
+ */
+
+
+?> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="/wp-content/themes/xdigital/landing.css">
+<div class="landing-page-wrapper">
+        <div class="main-wrapper">
+            <div class="container">
+                <div class="logo-header">
+                    <a href="#"><img src="/wp-content/themes/xdigital/img/logo.png" alt="Logo"></a>
+                </div>
+                <div class="main-section">
+                    <div class="case-study">
+                        <h1>
+                            <strong>Your Checklist</strong>
+
+                            for Higher & Continuing Ed
+                            Marketing Best Practices
+                        </h1>
+                        <ul>
+
+                            <li>Learn how seasoned marketers have excelled in marketing their institutions:</li>
+                            <li>How to build awareness;</li>
+                            <li>How to increase enrollment;</li>
+                            <li>Tactics for higher conversions in one month.</li>
+                        </ul>
+                        <strong class="headline">
+                            “See how UCSD Ext
+                            saw a 210% increase
+                            in conversions”
+                        </strong>
+                        <a href="#" class="btn">Case Study</a>
+                    </div>
+                    <div class="case-study0">
+                        <div class="img-wrapper"><img src="/wp-content/themes/xdigital/img/checklist.png" alt="Main-Image"><a href="#"
+                                download>Downloadable
+                                Checklist </a></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="form-section">
+            <form action="#">
+                <h2 style="text-align: center;">
+                    Learn More
+                </h2>
+                <div class="form-group">
+                    <input type="text" placeholder="Name First/Last">
+                    <input type="text" placeholder="Website URL (Required)">
+                    <input type="text" placeholder="Email (Required)">
+                    <input type="submit" value="Submit" class="btn">
+                </div>
+            </form>
+        </div>
+        <div class="footer-section">
+            <div class="container">
+                <div class="footer-wrapper">
+                    <a href="#"><img src="/wp-content/themes/xdigital/img/logo2.png" alt="logo"></a>
+                    <p>Innovation & data-driven digital marketing.</p>
+                    <ul class="social-icons">
+                        <li><a href="#"><img src="/wp-content/themes/xdigital/img/youtube.svg" alt="youtube"></a></li>
+                        <li><a href=""><img src="/wp-content/themes/xdigital/img/facebook.svg" alt="facebook"></a></li>
+                        <li><a href=""><img src="/wp-content/themes/xdigital/img/whatsapp.svg" alt="whatsapp"></a></li>
+
+                    </ul>
+                </div>
+                <span>Copyright © 10x 2022</span>
+            </div>
+        </div>
+    </div>
+
+<?php
+
+?>
